1da5a9f86da1218407549d395c7a90bdeb3eef0d
ducdn <ducdn@gmail.com>
2021-11-16 12:14:23 +0700

    update init code

--- boilerBlade/fe-boiler-blade/src/store.js
+++ boilerBlade/fe-boiler-blade/src/store.js
@@ -1,16 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { persistStore } from 'redux-persist';
-import { routerMiddleware, connectRouter } from 'connected-react-router';
 import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 
-import { history } from './helper/request/api_client';
 // import slide
 import home from './page/home/state';
 import app from './app_state/login';
 // REDUX
 const rootReducer = combineReducers({
-  router: connectRouter(history),
   home,
   app
 })
